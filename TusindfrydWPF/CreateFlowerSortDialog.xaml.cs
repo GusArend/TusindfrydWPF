dc6a4a83ce7c49cefb2340f1411b6f0b46401e36
Gustav Rasmussen <gustavarend@me.com>
2024-04-30 13:59:36 +0200

    Add project files.

---
+++ TusindfrydWPF/CreateFlowerSortDialog.xaml.cs
@@ -0,0 +1,85 @@
+﻿
+using System.Windows;
+
+
+namespace TusindfrydWPF
+{
+    /// <summary>
+    /// Interaction logic for CreateFlowerSortDialog.xaml
+    /// </summary>
+    public partial class CreateFlowerSortDialog : Window
+    {
+        Flowersort flowersort;
+
+        public CreateFlowerSortDialog()
+        {
+            InitializeComponent();
+            flowersort = new Flowersort();
+        }
+
+        private void SetFlowerSortProperties()
+        {
+            if (tbName.Text != "" && tbImagePath.Text != "" && tbProdTime.Text != "" && tbHalfLifeTime.Text != "" && tbSize.Text != "")
+            {
+                flowersort.Name = tbName.Text.Trim();
+                flowersort.PicturePath = tbImagePath.Text.Trim();
+                flowersort.ProductionTime = ParseInt(tbProdTime.Text.Trim());
+                flowersort.HalfLifeTime = ParseInt(tbHalfLifeTime.Text.Trim());
+                flowersort.Size = ParseInt(tbSize.Text.Trim());
+            } else
+            {
+                this.DialogResult = false;
+                this.Close();
+            }
+           
+        }
+
+        private int ParseInt(string text)
+        {
+            int result;
+            if (!int.TryParse(text, out result))
+            {
+                result = 0;
+            }
+            return result;
+        }
+
+        public new Flowersort ShowDialog()
+        {
+            btnOK.IsEnabled = false;
+            base.ShowDialog();
+            return flowersort;
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            SetFlowerSortProperties();
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void tb_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            EnableOkBtn();
+        }
+
+        private void EnableOkBtn()
+        {
+            if (tbName.Text != "" && tbImagePath.Text != "" && tbProdTime.Text != "" && tbHalfLifeTime.Text != "" && tbSize.Text != "")
+            {
+                btnOK.IsEnabled = true;
+            }
+            else
+            {
+                btnOK.IsEnabled = false;
+            }
+        }
+    }
+
+}
---
+++ TusindfrydWPF/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TusindfrydWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Flowersort> flowersorts;
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            flowersorts = new List<Flowersort>();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CreateFlowerSortDialog createFlowerSortDialog = new CreateFlowerSortDialog();
+            try
+            {
+                Flowersort flowersort = createFlowerSortDialog.ShowDialog();
+                if (flowersort != null)
+                {
+                    if (new string(flowersort.Name).Trim() != "" && new string(flowersort.PicturePath).Trim() != "" && new string(flowersort.ProductionTime.ToString()).Trim()
+                        != "" && new string(flowersort.HalfLifeTime.ToString()).Trim() != "" && new string(flowersort.Size.ToString()).Trim() != "")
+                    {
+                        flowersorts.Add(flowersort);
+                        UpdateFlowersortList();
+                        ShowStatusMessage("Flowersort added successfully.", "green");
+                    } else
+                    {
+                        ShowStatusMessage("Cancelled.", "blue");
+                    }
+                    
+                    
+                }
+                else
+                {
+                    ShowStatusMessage("Failed to add flowersort.", "red");
+                }
+            } catch (Exception ex)
+            {
+                ShowStatusMessage(ex.Message, "red");
+            }
+        }
+
+        private void UpdateFlowersortList()
+        {
+            tblockFlowersorts.Text = string.Join(Environment.NewLine, flowersorts.Select(fs => fs.Name));
+        }
+
+        private void ShowStatusMessage(string message, string color)
+        {
+            switch (color.ToLower())
+            {
+                case "red":
+                    statusLabel.Foreground = Brushes.Red;
+                    break;
+                case "green":
+                    statusLabel.Foreground = Brushes.Green;
+                    break;
+                case "blue":
+                    statusLabel.Foreground = Brushes.Blue;
+                    break;
+                default:
+                    statusLabel.Foreground = Brushes.Black;
+                    break;
+            }
+            statusLabel.HorizontalContentAlignment = HorizontalAlignment.Center;
+            statusLabel.Content = message;
+        }
+    }
+
+}
